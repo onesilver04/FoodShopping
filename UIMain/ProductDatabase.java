4503611b6545448af12bf9aae4e9df94c299e892
정채리 <165640490+jchaerry@users.noreply.github.com>
2024-07-08 20:53:12 +0900

    Update ProductDatabase.java

--- UIMain/ProductDatabase.java
+++ UIMain/ProductDatabase.java
@@ -5,7 +5,7 @@ public class ProductDatabase {
         products = new Product[9]; 
 
         // 각 상품을 생성하고 배열에 추가
-		products[0] = new Product("냉동삼겹살", 9900, 8800, 3, "/images/냉삼.jpg");
+		products[0] = new Product("냉동삼겹살", 9900, 8800, 3, "/images/냉삼.jpg"); //절대경로로 바꾸기
         products[1] = new Product("쌀", 26900, 15900, 5, "/images/쌀.jpg");
         products[2] = new Product("생수", 7600, 5880, 6, "/images/물.jpg");
         products[3] = new Product("고등어", 13900, 8340, 2, "/images/고등어.jpg");
